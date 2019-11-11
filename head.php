7a85123511206cc3c5a887e8851cce0a6dd7d4df
webable <webable@paran.com>
2019-11-11 14:48:06 +0900

    Update head.php

--- head.php
+++ head.php
@@ -5,4 +5,5 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>닷홈 홈페이지</title>
 </head>
